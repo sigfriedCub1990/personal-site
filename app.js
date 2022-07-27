48506db6c10c6503de0bdc28b56eacf01e1b0902
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-07-27 23:57:54 +0300

    log stuff if developing

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger('dev'));
+}
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
