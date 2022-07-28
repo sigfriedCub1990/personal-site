23ddad8bf2c5aaaa4bcd99381ef3c6ed4933cfe7
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-07-28 15:04:00 +0300

    put init function inside IIFE

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,27 @@
-function loadImage(id, selector) {
-    var el = document.getElementById(id);
-    var targetEl = selector ? document.querySelector(selector): el;
-    var imageToLoad;
+(function(document) {
+  function loadImage(id, selector) {
+      var el = document.getElementById(id);
+      var targetEl = selector ? document.querySelector(selector): el;
+      var imageToLoad;
 
-    if (el.dataset.image) {
-        imageToLoad = el.dataset.image;
-    } else if (typeof el.currentSrc === 'undefined') {
-        imageToLoad = el.src;
-    } else {
-        imageToLoad = el.currentSrc;
-    }
+      if (el.dataset.image) {
+          imageToLoad = el.dataset.image;
+      } else if (typeof el.currentSrc === 'undefined') {
+          imageToLoad = el.src;
+      } else {
+          imageToLoad = el.currentSrc;
+      }
 
-    if (imageToLoad) {
-        var img = new Image();
-        img.src = imageToLoad;
-        img.onload = function () {
-            targetEl.classList.add('is-loaded');
-        }
-    }
-}
+      if (imageToLoad) {
+          var img = new Image();
+          img.src = imageToLoad;
+          img.addEventListener('load', () => {
+              targetEl.classList.add('is-loaded');
+          });
+      }
+  }
 
-document.addEventListener('DOMContentLoaded', function() {
-    loadImage('wallpaper');
-    loadImage('pictureImage', '.picture');
-});
+  document.addEventListener('DOMContentLoaded', function() {
+      loadImage('wallpaper');
+  });
+})(document)
