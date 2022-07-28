50321b30447a5996dda67759eeb66caff53524bd
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-07-28 15:04:40 +0300

    pass nonce to view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(_, res, _) {
-  res.render('index', { title: 'Rubén Salvador - Software Engineer' });
+  const nonce = res.locals.cspNonce;
+  res.render('index', { title: 'Rubén Salvador - Software Engineer', nonce });
 });
 
 module.exports = router;
