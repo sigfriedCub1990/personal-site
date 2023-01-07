f0f85f1151fef527b9f8555fd252d5396c1ad71a
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-07 19:17:26 +0300

    build: Remove cookie-parser dependency

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const configureScp = require('./configure_scp')
+const configureScp = require('./configure_scp');
 
 const indexRouter = require('./routes/index');
 
@@ -22,25 +21,24 @@ configureScp(app);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(_, _, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, _) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
