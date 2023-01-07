7c3807c484c29d76d623753f742de8183963c4bc
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-01-07 21:11:34 +0300

    fix: Async image loading

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,30 @@
 (function(document) {
-  function loadImage(id, selector) {
-      var el = document.getElementById(id);
-      var targetEl = selector ? document.querySelector(selector): el;
-      var imageToLoad;
+    function loadImage(id, selector) {
+        var el = document.getElementById(id);
+        var targetEl = selector ? document.querySelector(selector) : el;
+        var imageToLoad;
 
-      if (el.dataset.image) {
-          imageToLoad = el.dataset.image;
-      } else if (typeof el.currentSrc === 'undefined') {
-          imageToLoad = el.src;
-      } else {
-          imageToLoad = el.currentSrc;
-      }
+        if (el.dataset.image) {
+            imageToLoad = el.dataset.image;
+        } else if (typeof el.currentSrc === 'undefined') {
+            imageToLoad = el.src;
+        } else {
+            imageToLoad = el.currentSrc;
+        }
 
-      if (imageToLoad) {
-          var img = new Image();
-          img.src = imageToLoad;
-          img.addEventListener('load', () => {
-              targetEl.classList.add('is-loaded');
-          });
-      }
-  }
-
-  document.addEventListener('DOMContentLoaded', function() {
-      loadImage('wallpaper');
-  });
-})(document)
+        if (imageToLoad) {
+            var img = new Image();
+            img.src = imageToLoad;
+            img.addEventListener('load', () => {
+                targetEl.classList.add('is-loaded');
+            });
+        }
+    }
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', function() {
+            loadImage('wallpaper');
+        });
+    } else {
+        loadImage('wallpaper');
+    }
+})(document);
